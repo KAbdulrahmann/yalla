2662309dd9bca87ec2d6f4a74681141c4b9fe9c6
khaled <khaledabdo7777@gmail.com>
2024-07-24 02:21:34 +0300

    added some config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/KAbdulrahmann/yalla/"
-})
+  base: '/yalla/',
+  build: {
+    rollupOptions: {
+      // Add any needed external modules here
+    },
+  },
+});
